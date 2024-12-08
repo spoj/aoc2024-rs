14a4a98819dabab82ad3cb9f12ad34773d6edf56
Matthew Wong <yuyeung.wong@gmail.com>
2024-12-08 15:06:26 +0800

    clean d1

--- src/d01.rs
+++ src/d01.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
-use std::collections::{BinaryHeap, HashMap};
 
 pub static SAMPLE: &str = r#"3   4
 4   3
@@ -7,46 +8,28 @@ pub static SAMPLE: &str = r#"3   4
 1   3
 3   9
 3   3"#;
-
 pub static INPUT: &str = include_str!("../data/d01.txt");
 
 pub fn part1(input: &str) {
-    let mut a: BinaryHeap<isize> = BinaryHeap::new();
-    let mut b: BinaryHeap<isize> = BinaryHeap::new();
-    input.lines().for_each(|x| {
-        let vec = x
-            .split_ascii_whitespace()
-            .map(|num_as_str| num_as_str.parse().unwrap())
-            .take(2)
-            .collect_vec();
-        a.push(vec[0]);
-        b.push(vec[1]);
+    let mut a: Vec<isize> = Vec::new();
+    let mut b: Vec<isize> = Vec::new();
+    input.split_ascii_whitespace().tuples().for_each(|(x, y)| {
+        a.push(x.parse().unwrap());
+        b.push(y.parse().unwrap());
     });
-    let day1_part1: isize = a.into_iter().zip(b).map(|(a, b)| (a - b).abs()).sum();
+    a.sort();
+    b.sort();
+    let day1_part1: isize = a.into_iter().zip(b).map(|(x, y)| (x - y).abs()).sum();
     dbg!(day1_part1);
 }
-
 pub fn part2(input: &str) {
-    let mut left: Vec<isize> = vec![];
-    let mut freq_r: HashMap<isize, isize> = HashMap::new();
-    input.lines().for_each(|x| {
-        let vec = x
-            .split_ascii_whitespace()
-            .map(|num_as_str| num_as_str.parse().unwrap())
-            .take(2)
-            .collect_vec();
-        left.push(vec[0]);
-        let right_num = vec[1];
-        let e = freq_r.entry(right_num).or_default();
-        *e += 1
+    let mut a: Vec<isize> = Vec::new();
+    let mut b: HashMap<isize, isize> = HashMap::new();
+    input.split_ascii_whitespace().tuples().for_each(|(x, y)| {
+        a.push(x.parse().unwrap());
+        let e = b.entry(y.parse().unwrap());
+        *e.or_default() += 1;
     });
-
-    let day1_part2: isize = left
-        .iter()
-        .map(|n| {
-            let freq = freq_r.get(n).unwrap_or(&0);
-            freq * n
-        })
-        .sum();
-    dbg!(day1_part2);
+    let ans: isize = a.into_iter().map(|x| x * b.get(&x).unwrap_or(&0)).sum();
+    dbg!(ans);
 }
