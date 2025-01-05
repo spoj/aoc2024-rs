a8cfa8fba626480b153cd30e149aa43f7f60e412
Matthew Wong <yuyeung.wong@gmail.com>
2025-01-05 22:15:03 +0800

    updates

--- src/main.rs
+++ src/main.rs
@@ -6,75 +6,99 @@ use aoc2024_rs::*;
 fn main() {
     timed_print(1, 1, || d01::part1(d01::INPUT));
     timed_print(1, 2, || d01::part2(d01::INPUT));
+    println!();
 
     timed_print(2, 1, || d02::part1(d02::INPUT));
     timed_print(2, 2, || d02::part2(d02::INPUT));
+    println!();
 
     timed_print(3, 1, || d03::part1(d03::INPUT));
     timed_print(3, 2, || d03::part2(d03::INPUT));
+    println!();
 
     timed_print(4, 1, || d04::part1(d04::INPUT));
     timed_print(4, 2, || d04::part2(d04::INPUT));
+    println!();
 
     timed_print(5, 1, || d05::part1(d05::INPUT));
     timed_print(5, 2, || d05::part2(d05::INPUT));
+    println!();
 
     timed_print(6, 1, || d06::part1(d06::INPUT));
     timed_print(6, 2, || d06::part2(d06::INPUT));
+    println!();
 
     timed_print(7, 1, || d07::part1(d07::INPUT));
     timed_print(7, 2, || d07::part2(d07::INPUT));
+    println!();
 
     timed_print(8, 1, || d08::part1(d08::INPUT));
     timed_print(8, 2, || d08::part2(d08::INPUT));
+    println!();
 
     timed_print(9, 1, || d09::part1(d09::INPUT));
     timed_print(9, 2, || d09::part2(d09::INPUT));
+    println!();
 
     timed_print(10, 1, || d10::part1(d10::INPUT));
     timed_print(10, 2, || d10::part2(d10::INPUT));
+    println!();
 
     timed_print(11, 1, || d11::part1(d11::INPUT));
     timed_print(11, 2, || d11::part2(d11::INPUT));
+    println!();
 
     timed_print(12, 1, || d12::part1(d12::INPUT));
     timed_print(12, 2, || d12::part2(d12::INPUT));
+    println!();
 
     timed_print(13, 1, || d13::part1(d13::INPUT));
     timed_print(13, 2, || d13::part2(d13::INPUT));
+    println!();
 
     timed_print(14, 1, || d14::part1(101, 103, 100, d14::INPUT));
     timed_print(14, 2, || d14::part2(101, 103, d14::INPUT));
+    println!();
 
     timed_print(15, 1, || d15::part1(d15::INPUT));
     timed_print(15, 2, || d15::part2(d15::INPUT));
+    println!();
 
     timed_print(16, 1, || d16::part1(d16::INPUT));
     timed_print(16, 2, || d16::part2(d16::INPUT));
+    println!();
 
     timed_print(17, 1, || d17::part1(d17::INPUT));
     timed_print(17, 2, || d17::part2(d17::INPUT));
+    println!();
 
     timed_print(18, 1, || d18::part1(71, 71, 1024, d18::INPUT));
     timed_print(18, 2, || d18::part2(71, 71, d18::INPUT));
+    println!();
 
     timed_print(19, 1, || d19::part1(d19::INPUT));
     timed_print(19, 2, || d19::part2(d19::INPUT));
+    println!();
 
     timed_print(20, 1, || d20::part1(100, d20::INPUT));
     timed_print(20, 2, || d20::part2(100, d20::INPUT));
+    println!();
 
     timed_print(21, 1, || d21::part1(d21::INPUT));
     timed_print(21, 2, || d21::part2(d21::INPUT));
+    println!();
 
     timed_print(22, 1, || d22::part1(d22::INPUT));
     timed_print(22, 2, || d22::part2(d22::INPUT));
+    println!();
 
     timed_print(23, 1, || d23::part1(d23::INPUT));
     timed_print(23, 2, || d23::part2(d23::INPUT));
+    println!();
 
     timed_print(24, 1, || d24::part1(d24::INPUT));
     timed_print(24, 2, || d24::part2(d24::INPUT));
+    println!();
 
     timed_print(25, 1, || d25::part1(d25::INPUT));
 }
